1487bc23785302d122cf98fb5793160f018dd05f
githeed <tmdgmldhk11@gmail.com>
2024-07-24 17:38:14 +0900

    overtlapSphere

--- Assets/HSH/Scripts/H_PlayerAttack.cs
+++ Assets/HSH/Scripts/H_PlayerAttack.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using UnityEngine;
 
 public class H_PlayerAttack : MonoBehaviour
@@ -9,12 +11,10 @@ public class H_PlayerAttack : MonoBehaviour
     public float attTime = 5;
     private float curAttTime = 0;
 
-    public float scanRange = 5f;
+    public float scanRange = 10f;
     public LayerMask targetLayer;
     public Collider[] targets;
     public Transform nearestTarget;
-
-
 
     // Start is called before the first frame update
     void Start()
@@ -25,7 +25,7 @@ public class H_PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        targets = Physics.OverlapSphere(transform.position, scanRange, targetLayer);
+        BasicAttack();
     }
 
     Transform GetNearest()
@@ -33,6 +33,11 @@ public class H_PlayerAttack : MonoBehaviour
         Transform result = null;
         float dist = 9999f;
 
+        //Stopwatch stopwatch = new Stopwatch();
+        //stopwatch.Start();
+
+
+        // 구에 오버랩된 게임오브젝트 중에 가장 가까운 놈의 위치 찾기
         foreach(Collider target in targets)
         {
             float curDist = Vector3.Distance(transform.position, target.transform.position);
@@ -42,19 +47,34 @@ public class H_PlayerAttack : MonoBehaviour
                 result = target.transform;
             }
         }
+
+        //print(stopwatch.ElapsedMilliseconds);
+        //print(stopwatch.Elapsed);
+        //print(stopwatch.ElapsedTicks);
+        //stopwatch.Stop();
         return result;
     }
 
     void BasicAttack()
     {
-        nearestTarget = GetNearest();
-
+        // 5초마다
         curAttTime += Time.deltaTime;
-
         if(curAttTime > attTime)
         {
+            // 오버랩 스피어
+            targets = Physics.OverlapSphere(transform.position, scanRange, targetLayer);
+            nearestTarget = GetNearest();
 
+            // 공격하기
+            if (nearestTarget.gameObject != null)
+            {
+                print(nearestTarget.gameObject);
+            }
+            else
+            {
+                print("null");
+            }
             curAttTime = 0;
-        }
+        } 
     }
 }
