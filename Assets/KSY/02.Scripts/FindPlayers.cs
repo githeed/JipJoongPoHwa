5106ee020d665bcac843743d4efd4109337c243d
KwonSiyoon <140003447+KwonSiyoon@users.noreply.github.com>
2024-07-31 17:22:48 +0900

    0731

---
+++ Assets/KSY/02.Scripts/FindPlayers.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindPlayers : MonoBehaviour
+{
+    public GameObject player0;
+    public GameObject player1;
+    public GameObject target;
+    protected float dist0;
+    protected float dist1;
+
+    private void Awake()
+    {
+        player0 = GameObject.FindWithTag("Player");
+        player1 = GameObject.FindWithTag("Player1");
+    }
+    void Update()
+    {
+        dist0 = (player0.transform.position - transform.position).magnitude;
+        if (player1 != null && player1.activeSelf)
+        {
+            dist1 = (player1.transform.position - transform.position).magnitude;
+            if (dist0 < dist1)
+            {
+                target = player0;
+            }
+            else
+            {
+                target = player1;
+            }
+        }
+        if (player1 == null) target = player0;
+        if (player1 != null && !player1.activeSelf && player0 != null) target = player0;
+    }
+}
--- Assets/KSY/02.Scripts/Nokturne.cs
+++ Assets/KSY/02.Scripts/Nokturne.cs
@@ -5,10 +5,20 @@ using UnityEngine.AI;
 
 public class Nokturne : MonoBehaviour
 {
-    GameObject target;
+    public enum NokturneState
+    {
+        IDLE,
+        MOVE,
+        ATTACK,
+        DEAD
+    }
+    FindPlayers findPlayers;
+    NokturneState currState;
     public GameObject attackIndicatorPos;
+    public GameObject target;
+    NavMeshAgent agent;
     Vector3 dir;
-    Vector3 forwardDir;
+    Vector3 attackDir;
     public float attackDelay;
     WaitForSeconds attackDelays;
     Ray toTarget;
@@ -17,50 +27,125 @@ public class Nokturne : MonoBehaviour
     bool attacking;
     public float attackSpeed;
     Vector3 destination;
-    NavMeshAgent agent;
+    Coroutine attackCoroutine;
+    float attackRange = 10;
+    float moveDist;
 
     // Start is called before the first frame update
     void Start()
     {
+        currState = NokturneState.IDLE;
         attackIndicatorPos.SetActive(false);
+        findPlayers = GetComponent<FindPlayers>();
         attackDelays = new WaitForSeconds(attackDelay);
-        canAttack = true;
         agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        target = GetComponent<EnemyMove>().target;
-        dir = target.transform.position - transform.position;
+        target = findPlayers.target;
+        if (target != null)
+        {
+            dir = target.transform.position - transform.position;
+        }
         toTarget = new Ray(transform.position, dir);
-        if(Physics.Raycast(toTarget, out hitInfo, 5, 1 << LayerMask.NameToLayer("Player")))
+        canAttack = Physics.Raycast(toTarget, out hitInfo, attackRange, 1 << LayerMask.NameToLayer("Player"));
+        print("업데이트에서" + agent.destination);
+        switch (currState)
         {
-            StartCoroutine(NocturneAttack());
+            case NokturneState.IDLE:
+                UpdateIdle();
+                break;
+            case NokturneState.MOVE:
+                UpdateMove();
+                break;
+            case NokturneState.ATTACK:
+                UpdateAttack();
+                break;
+            case NokturneState.DEAD:
+                break;
+            default:
+                break;
         }
-        if (attacking) 
-        {
-            transform.Translate(dir.normalized * attackSpeed * Time.deltaTime);
-        }
-
-
 
 
     }
 
+
+    public void ChangeState(NokturneState state)
+    {
+        print(currState + "------>" + state);
+
+
+        currState = state;
+
+
+        switch (currState)
+        {
+            case NokturneState.IDLE:
+                break;
+            case NokturneState.MOVE:
+                OnMove();
+                break;
+            case NokturneState.ATTACK:
+                OnAttack();
+                break;
+            case NokturneState.DEAD:
+                break;
+            default:
+                break;
+        }
+    }
+
+    void UpdateIdle()
+    {
+        ChangeState(NokturneState.MOVE);
+    }
+    void UpdateMove()
+    {
+        if(canAttack)
+        {
+            ChangeState(NokturneState.ATTACK);
+        }
+    }
+    void OnAttack()
+    {
+        StartCoroutine(NocturneAttack());
+    }
+
+    void UpdateAttack()
+    {
+        if(Mathf.Approximately((agent.destination-transform.position).magnitude, 0.1f))
+        {
+            if (dir.magnitude < attackRange)
+            {
+                OnAttack();
+            }
+            else
+            {
+                ChangeState(NokturneState.MOVE);
+            }
+        }
+    }
+
+    public void OnMove()
+    {
+        agent.destination = target.transform.position;
+    }
+
+
     IEnumerator NocturneAttack()
     {
-        if (canAttack)
-        {
-            canAttack = false;
-            agent.enabled = false;
-            destination = target.transform.position;
-            attackIndicatorPos.SetActive(true);
-            attackIndicatorPos.transform.forward = new Vector3(dir.normalized.x, 0, dir.normalized.z);
-            yield return attackDelays;
-
-        }
-        canAttack = true;
-
+        print("코루틴 부름");
+        agent.destination = transform.position + dir.normalized * attackRange;
+        print("코루틴에서" + agent.destination);
+        attackDir = agent.destination - transform.position;
+        agent.speed = 0;
+        attackIndicatorPos.SetActive(true);
+        attackIndicatorPos.transform.forward = new Vector3(attackDir.x, 0, attackDir.z);
+        yield return attackDelays;
+        agent.speed = attackSpeed;
+        
     }
 }
