e3dd7f0b38d2170e052533938824ce5e40afcbf4
KwonSiyoon <140003447+KwonSiyoon@users.noreply.github.com>
2024-08-08 16:33:27 +0900

    11

---
+++ Assets/KSY/02.Scripts/IAnimatorInterface.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IAnimatorInterface
+{
+    public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex);
+}
