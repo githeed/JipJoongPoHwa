4985670c450d458a02e29f8118e80b467428dc46
anditsoon <83330939+anditsoon@users.noreply.github.com>
2024-08-14 12:37:42 +0900

    feather 스크립트 따로 만듦

---
+++ Assets/YDW/Scripts/Y_Feather.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+
+
+public class Y_Feather : MonoBehaviour
+{
+    GameObject ally;
+    public IObjectPool<GameObject> pool;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ally = GameObject.Find("Ally");
+        pool = ally.GetComponent<Y_PlayerAttack>().pool;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StopDestroy()
+    {
+        StopAllCoroutines();
+    }
+
+    public void StartDestroy(float time)
+    {
+        StartCoroutine(AutoDestroy(time));
+    }
+
+    IEnumerator AutoDestroy(float time)
+    {
+        yield return new WaitForSeconds(time);
+        pool.Release(gameObject);
+    }
+}
