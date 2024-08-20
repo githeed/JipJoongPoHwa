102891f414ef4999f850552b0374d6ca78352c9e
KwonSiyoon <140003447+KwonSiyoon@users.noreply.github.com>
2024-08-20 11:38:56 +0900

    11

---
+++ Assets/KSY/02.Scripts/TestParticle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestParticle : MonoBehaviour
+{
+    ParticleSystem ps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnParticleTrigger()
+    {
+        print("트리거됨.");
+    }
+
+    
+
+    private void OnParticleCollision(GameObject other)
+    {
+        print(other.name);
+    }
+}
